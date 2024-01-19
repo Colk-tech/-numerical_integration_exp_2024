2c1557bdc51d94e3a37de042e543e9792578d0fb
Colk-tech <master@colk.dev>
2024-01-19 13:48:20 +0900

    add: add `calculate_polynomial_double()` in `ni_exp_util.c`

--- ni_exp_util.c
+++ ni_exp_util.c
@@ -16,4 +16,15 @@ float *calculate_polynomial(float (*func_ptr)(float), float a, float b, int n, f
     return y;
 }
 
+double *calculate_polynomial_double(double (*func_ptr)(double), double a, double b, int n, double *y) {
+    double h = (b - a) / (double) n;
+
+    for (int i = 0; i < n; i++) {
+        double x = a + (h * (double) i);
+        y[i] = func_ptr(x);
+    }
+
+    return y;
+}
+
 #endif // NI_EXP_UTIL_C
